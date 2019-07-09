94f2d9e893049b7a5a13d202d048a25e936cd0cd
VaiariiTepa <tepavaiarii@gmail.com>
2019-07-09 10:44:19 -1000

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,68 @@
+
+// var tableau = [1,2,3,4,5];
+var tableau = [[1,2,3,4,5],[0,9,13,7,6]];
+var i=0;
+var f=0;
+
+function launch(){
+    for(i;i<tableau.length;i++){
+            f= 0;
+        for(f;f<=tableau[i][f];f++){
+            console.log(i+1+" tableau "+tableau[i][f]);
+        
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // console.log('2em tableau '+tableau[i][f]);
+            // if(tableau[i][f] == 7){
+            //     tableau[i][f]=tableau[i][f]%2;
+            //     console.log("Modulo 2/7 "+tableau[i][f]);
+            // }
+
+
+
+
+
+
+
+
+        // for(i;i<tableau.length;i++){
+        //     if(tableau[i] == 3){
+        //         console.log('c\'est un '+tableau[i]+', alors je rajoute 1');
+        //         console.log('Modulo '+tableau[i]%2);
+        //         tableau[i]++;
+        //         console.log('c\'est devenu un '+ tableau[i]);
+                
+        //     }else{
+
+        //         console.log('Affichage normal '+tableau[i]);
+
+        //     }
+
+
+        // }
+
+
+    //     console.log(tableau[i][f]);
